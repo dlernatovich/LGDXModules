eb882f79d92f9b5be0292872e76bd7d55f55f199
dlernatovich <dlernatovich@geeksforless.net>
2014-09-10 21:47:35 +0300

    Minor fixes

--- LibGDXGradle/core/src/com/artlite/libgdxgame/main/MyGdxGame.java
+++ LibGDXGradle/core/src/com/artlite/libgdxgame/main/MyGdxGame.java
@@ -1,58 +1,38 @@
 package com.artlite.libgdxgame.main;
 
-import com.artlite.libgdxgame.constants.AnimationProjections;
-import com.artlite.libgdxgame.enteties.actors.SpriteSheetPlayer;
-import com.artlite.libgdxgame.enteties.actors.StaticActor;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.Touchable;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MyGdxGame extends BaseApplicationAdapter {
 
     private SpriteBatch batch;
     private Stage stage;
-    private StaticActor background;
-    private SpriteSheetPlayer player;
-
-    private final int X_DIVIDER = 6;
-    private final int Y_ADDITIONAL = 20;
-
-
-    private static Map<AnimationProjections, Integer> projectionsMap = new HashMap<AnimationProjections, Integer>();
-
-    static {
-        projectionsMap.put(AnimationProjections.FORWARD, 0);
-        projectionsMap.put(AnimationProjections.BACKWARD, 1);
-    }
 
     @Override
     public void create() {
         super.create();
         batch = new SpriteBatch();
         stage = new Stage();
-        background = new StaticActor("maingame/background.png", screen.getWidth(), screen.getHeigh());
-
-        player = new SpriteSheetPlayer("maingame/main_actor.png", 8, 2,
-                projectionsMap, 1);
-
-        int startX = (screen.getWidth() / X_DIVIDER);
-        int startY = (int) (screen.getHeigh() - (player.getPlayerHeigh() + Y_ADDITIONAL
-                * 1));
-
-        player.setDrawX(startX);
-        player.setDrawY(startY);
-
-        player.setTouchable(Touchable.enabled);
-        setInputProcessor(player);
-
-
-        stage.addActor(background);
-        stage.addActor(player);
+//        background = new StaticActor("maingame/background.png", screen.getWidth(), screen.getHeigh());
+//
+//        player = new SpriteSheetPlayer("maingame/main_actor.png", 8, 2,
+//                projectionsMap, 1);
+//
+//        int startX = (screen.getWidth() / X_DIVIDER);
+//        int startY = (int) (screen.getHeigh() - (player.getPlayerHeigh() + Y_ADDITIONAL
+//                * 1));
+//
+//        player.setDrawX(startX);
+//        player.setDrawY(startY);
+//
+//        player.setTouchable(Touchable.enabled);
+//        setInputProcessor(player);
+//
+//
+//        stage.addActor(background);
+//        stage.addActor(player);
 
     }
 
